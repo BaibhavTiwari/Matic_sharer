eeff9a26f1b6118e0a4c036851cf4ce695afe2f0
baibhav <baibhavtiwari37@gmail.com>
2023-02-18 14:57:24 +0530

    done ✅

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,9 +5,9 @@ import logo from "../../images/logo.png";
 const Footer = () => (
     <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
         <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
-            <div className="flex flex-[0.5] justify-center items-center">
+            {/* <div className="flex flex-[0.5] justify-center items-center">
                 <img src={logo} alt="logo" className="w-32" />
-            </div>
+            </div> */}
 
         </div>
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,16 +13,16 @@ const Navbar = () => {
 
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+            {/* <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
-            </div>
+            </div> */}
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
                     <NavBarItem key={item + index} title={item} />
                 ))}
-                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                {/* <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
                     Login
-                </li>
+                </li> */}
             </ul>
             <div className="flex relative">
                 {!toggleMenu && (
