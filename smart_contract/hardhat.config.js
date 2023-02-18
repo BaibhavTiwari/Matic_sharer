6f263dcbf6322fe6fd7589027ba8c164d1c23e86
baibhav <baibhavtiwari37@gmail.com>
2023-02-18 14:35:44 +0530

    updated env

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     goerli:{
-      url: process.env.REACT_APP_KEY,
-      accounts:[process.env.REACT_APP_PRIVATE_KEY]
+      url:'https://goerli.infura.io/v3/99453d03151f437c94076ea56b955ce8',    
+      accounts:['d6072d71019098e120bb24684792d97eb0ffcb45b5c25c7d80c66e2f5834a9db']
     }, 
   },
 };
