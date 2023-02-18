4f9713a6f214ba99fb41a1c2f509a437b401ed63
baibhav <baibhavtiwari37@gmail.com>
2023-02-18 14:28:31 +0530

    updated

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,9 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
-    },
+    goerli:{
+      url: process.env.REACT_APP_KEY,
+      accounts:[process.env.REACT_APP_PRIVATE_KEY]
+    }, 
   },
 };
