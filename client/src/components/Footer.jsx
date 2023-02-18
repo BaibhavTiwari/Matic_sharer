4553fac7a23b6b5bc51472e9b9dbc0318ae131f6
baibhav <baibhavtiwari37@gmail.com>
2023-02-18 13:24:38 +0530

    completed

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import logo from "../../images/logo.png";
+
+const Footer = () => (
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+        <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+            <div className="flex flex-[0.5] justify-center items-center">
+                <img src={logo} alt="logo" className="w-32" />
+            </div>
+
+        </div>
+
+        <div className="flex justify-center items-center flex-col mt-5">
+            <p className="text-white text-base text-center mx-2 cursor-pointer">Made with love and Passion🚀</p>
+        </div>
+
+        <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+
+        <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+            <p className="text-white text-base text-center mx-2 cursor-pointer"><a href="https://baibhavtiwari37@gmail.com">G-MAIL</a></p>
+            <p className="text-white text-base text-center mx-2 cursor-pointer"><a href="https://github.com/baibhav-tiwari">GITHUB</a></p>
+
+        </div>
+    </div>
+);
+
+export default Footer;
